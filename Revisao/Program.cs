66bb485b76436bea959ec500672754e83abd0b87
dhgnogueira <denise_dhg@hotmail.com>
2022-03-27 23:28:17 -0300

    Primeiros passos em dotnet

--- Revisao/Program.cs
+++ Revisao/Program.cs
@@ -4,6 +4,8 @@
     {
         static void Main(string[] args)
         {
+            Aluno[] alunos = new Aluno[5];
+            var indiceAluno = 0;      
             string opcaoUsuario = ObterOpcaoUsuario();
 
             while (opcaoUsuario.ToUpper() != "X")
@@ -11,10 +13,33 @@
                 switch (opcaoUsuario)
                 {
                     case "1":
-                        //TODO: adicionar aluno
-                        break;
+                        Console.WriteLine("Informe o nome o aluno:");
+                        var aluno = new Aluno();
+                        aluno.Nome = Console.ReadLine();
+
+                        Console.WriteLine("Informe a nota do aluno:");
+
+                        if (decimal.TryParse(Console.ReadLine(), out decimal nota))
+                        {
+                            aluno.Nota = nota;  
+                        }
+                        else
+                        {
+                           throw new ArgumentException("O valor da nota deve ser decimal"); 
+                        }
+
+                        alunos[indiceAluno] = aluno;
+                        indiceAluno++;
+
+                        break;                     
                     case "2":
-                        //TODO: listar aluno
+                        foreach(var a in alunos)
+                        {
+                            if (!string.IsNullOrEmpty(a.Nome))
+                            {
+                                Console.WriteLine($"Aluno: {a.Nome} - Nota: {a.Nota}");
+                            }  
+                        }
                         break;
                     case "3":
                         //TODO: calcular media geral
@@ -29,6 +54,7 @@
 
         private static string ObterOpcaoUsuario()
         {
+            Console.WriteLine();
             Console.WriteLine("Informe a opção desejada:");
             Console.WriteLine("1 - Inserir novo aluno");
             Console.WriteLine("2 - Listar alunos");
@@ -37,7 +63,7 @@
             Console.WriteLine();
 
             string opcaoUsuario = Console.ReadLine();
-            
+            Console.WriteLine();
             return opcaoUsuario;
         }
     }
