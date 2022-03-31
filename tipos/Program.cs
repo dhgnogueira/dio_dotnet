fe18cc577412b9b0dc08f4b775ce999218b40cd0
dhgnogueira <denise_dhg@hotmail.com>
2022-03-31 17:20:37 -0300

    trabalhando com tipos de referencia

--- tipos/Program.cs
+++ tipos/Program.cs
@@ -1,16 +1,32 @@
 ﻿using static System.Console;
 public class Program
 {
+    static void Demo1()
+    {
+        int a = 2;
+       a = Adicionar20(a);
+
+       WriteLine($"O valor da variável a é {a}"); 
+    }
     static int Adicionar20(int x)
     {
         return x + 20;
     }
+
+    static void TrocarNome(Pessoa p1, string NomeNovo)
+    {
+        p1.Nome = NomeNovo;
+    }
     public static void Main()
     {
-       int a = 2;
-       a = Adicionar20(a);
+        Pessoa p1 = new Pessoa();
+        p1.Nome = "Ricardo";
+        p1.Idade = 30;
+        p1.Documento = "1234";
 
-       WriteLine($"O valor da variável a é {a}"); 
+        TrocarNome(p1, "José");
+
+        WriteLine($"O novo nome é: {p1.Nome}");
     }
 
 }
