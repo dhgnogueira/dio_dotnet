3018b04dd3a2163d67b79943846bcb1361ad19a2
dhgnogueira <denise_dhg@hotmail.com>
2022-03-31 12:59:41 -0300

    tipos referencia e valor

--- tipos/Program.cs
+++ tipos/Program.cs
@@ -1,9 +1,16 @@
 ﻿using static System.Console;
 public class Program
 {
+    static int Adicionar20(int x)
+    {
+        return x + 20;
+    }
     public static void Main()
     {
-       WriteLine("Hello World!"); 
+       int a = 2;
+       a = Adicionar20(a);
+
+       WriteLine($"O valor da variável a é {a}"); 
     }
 
 }
