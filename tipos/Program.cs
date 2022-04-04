7ef1f7746ac12997cb4422912bf7f689928a00fa
dhgnogueira <denise_dhg@hotmail.com>
2022-04-04 13:29:19 -0300

    Finalizando conceitos

--- tipos/Program.cs
+++ tipos/Program.cs
@@ -1,14 +1,38 @@
 ﻿using static System.Console;
 public class Program
 {
+     static void Demo6()
+     {
+         int[] numeros = new int[] {0,2,4,6,8};
+       WriteLine($"Digite o número que gostaria de encontrar: ");
+        var numero = int.Parse(ReadLine());
+        var idxEncontrado = EncontrarNumero(numeros,numero);
+
+        if(idxEncontrado >= 0)
+        {
+            WriteLine($"O número digitado está na posição: {idxEncontrado}");
+        }
+        else
+        {
+           WriteLine($"O número digitado não foi encontrado"); 
+        }
+     }
+    static void Demo5()
+    {
+        int[] pares = new int[]{0,2,4,6,8};
+
+      MudarParaImpar(pares);
+
+      WriteLine($"Os ímpares: {string.Join(",", pares)}"); 
+    }
+
+
     static void Demo4()
     {
         string nome = "Ricardo";
         TrocarNome(nome, "José");
         WriteLine($"O novo nome é: {nome}");
     }
-
-
 
     static void Demo3()
     {
@@ -83,13 +107,51 @@ public class Program
         }
 
     }
+
+    static int EncontrarNumero(int[] numeros, int numero)
+    {
+       for(int i = 0; i < numeros.Length; i++)
+       {
+          if(numeros[i] == numero)
+            return i; 
+       }
+       return -1;
+    }
+
+    static bool EncontrarPessoa(List<Pessoa> pessoas, Pessoa pessoa)
+    {
+        foreach (var item in pessoas)
+        {
+            if(item.Nome == pessoa.Nome)
+            {
+                return true;
+            }
+            
+        }
+        return false;
+    }
     public static void Main()
     {
-      int[] pares = new int[]{0,2,4,6,8};
+        List<Pessoa> pessoas = new List<Pessoa>()
+        {
+           new Pessoa(){Nome = "Ricardo"}, 
+           new Pessoa(){Nome = "José"},
+           new Pessoa(){Nome = "Maria"}, 
+           new Pessoa(){Nome = "Fabiana"}, 
+           new Pessoa(){Nome = "Eduardo"},  
+        };
 
-      MudarParaImpar(pares);
-
-      WriteLine($"Os ímpares: {string.Join(",", pares)}");  
-
+        WriteLine("Digite a pessoa que gostaria de localizar:");
+        var nome = ReadLine();
+        var pessoa = new Pessoa(){Nome = nome};
+        var encontrado = EncontrarPessoa(pessoas, pessoa);
+        if(encontrado)
+        {
+           WriteLine("Pessoa localizada!");  
+        }
+        else
+        {
+            WriteLine("Pessoa não localizada!"); 
+        }
     }
 }      
