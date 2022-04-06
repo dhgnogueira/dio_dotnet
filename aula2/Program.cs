a7f1a3b438d3982575f5601c5c7a99d2502bfbde
dhgnogueira <denise_dhg@hotmail.com>
2022-04-06 15:20:35 -0300

    array de string com nomes

--- aula2/Program.cs
+++ aula2/Program.cs
@@ -1,15 +1,45 @@
 ﻿using static System.Console;
 class Aula2
 {
+    static void Demo1()
+    
+    {
+         int a = 5;
+        Adicionar20(ref a);
+        WriteLine($"O valor de a é: {a}");
+    }
     static void Adicionar20(ref int a)
     {
        a += 20; 
     }
+    static void AlterarNome(string[] nomes, string nome, string nomeNovo)
+    {
+        for(int i = 0; i < nomes.Length; i++)
+        {
+            if(nomes[i] == nome)
+            {
+                nomes[i] = nomeNovo;
+            }
+        }
+    }
     static void Main()
     {
-        int a = 5;
-        Adicionar20(ref a);
-        WriteLine($"O valor de a é: {a}");
+       var nomes = new string[]{"José","Maria","Ricardo","Alice","Pedro"};
+        
+        WriteLine($@"A lista de nomes é: 
+        {string.Join(", \n",nomes)}"
+        );
+
+        WriteLine("Digite o nome a ser substituído:");
+        var nome = ReadLine();
+        WriteLine("Digite o nome novo:");
+        var nomeNovo = ReadLine();
+        
+        AlterarNome(nomes, nome, nomeNovo);
+        
+        WriteLine($@"A lista de nomes alterada é: 
+        {string.Join(",\n",nomes)}
+        ");
     }
 }
     
