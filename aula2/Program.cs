6777fec9298914512f4c415b13410a33f6b34011
dhgnogueira <denise_dhg@hotmail.com>
2022-04-06 14:16:58 -0300

    exemplo com ref

---
+++ aula2/Program.cs
@@ -0,0 +1,16 @@
+﻿using static System.Console;
+class Aula2
+{
+    static void Adicionar20(ref int a)
+    {
+       a += 20; 
+    }
+    static void Main()
+    {
+        int a = 5;
+        Adicionar20(ref a);
+        WriteLine($"O valor de a é: {a}");
+    }
+}
+    
+
