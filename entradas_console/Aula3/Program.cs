2575546b341a4764aa4740068992b798356609aa
dhgnogueira <denise_dhg@hotmail.com>
2022-04-08 23:18:04 -0300

    Comandos de repetição for, foreach, do e while

---
+++ entradas_console/Aula3/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Aula3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] lista = {1,2,3,4,5};
+
+            foreach(int numero in lista)
+            {
+               Console.WriteLine(numero); 
+            }
+            
+            // int valor;
+            // Console.WriteLine("Digite um valor: ");
+            // valor = int.Parse(Console.ReadLine());
+
+            // for(int i = valor; i <= 10; i++)
+            // {
+            //     Console.WriteLine(i);
+            // }
+
+            // Boolean condicao = true;
+            // int valor;
+
+            // do
+            // {
+            //     Console.WriteLine("Digite um valor, 0 para sair");
+            //     valor = int.Parse(Console.ReadLine());
+            //         if(valor == 0)
+            //         {
+            //            Console.WriteLine("Você saiu da aplicação!"); 
+            //            condicao = false;
+            //         }
+            //         else
+            //         {
+            //            Console.WriteLine("O valor informado é: " + valor); 
+            //         }
+            // }while(condicao == true);
+            
+        }
+    }
+}
