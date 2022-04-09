24eabd1f924fc97e75773c2fea8a49e348d7ad29
dhgnogueira <denise_dhg@hotmail.com>
2022-04-09 13:33:27 -0300

    Pulando com break e continue

---
+++ entradas_console/Aula4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Aula4
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+           Boolean condicao = true;
+            int valor;
+
+            do
+            {
+                Console.WriteLine("Digite um valor, 0 para sair");
+                valor = int.Parse(Console.ReadLine());
+                   
+                    if(valor == 0)
+                    {
+                       Console.WriteLine("Você saiu da aplicação!"); 
+                       break;
+                    }
+                    else
+                    {
+                       Console.WriteLine("O valor informado é: " + valor);
+                       continue; 
+                    }
+            }while(condicao == true); 
+        }
+    }
+}
