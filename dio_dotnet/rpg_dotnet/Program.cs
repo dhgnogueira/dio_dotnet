6f7d881c68d0f106b3b98e5b3330442c716a9a85
dhgnogueira <denise_dhg@hotmail.com>
2022-04-10 00:42:37 -0300

    rpg dotnet

---
+++ dio_dotnet/rpg_dotnet/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace dotnet_poo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hero arus = new Hero("Arus", 23, "knight");
+            Wizard wizard = new Wizard("Jennica", 23, "White Wizard");
+            
+            Console.WriteLine(arus.Attack());
+            Console.WriteLine(wizard.Attack());
+        }
+    }
+}
---
+++ dio_dotnet/rpg_dotnet/entities/Hero.cs
@@ -0,0 +1,36 @@
+using System;
+namespace rpg_dotnet.entities
+{
+    public class Hero
+    {
+       public Hero(string Name, int level, string HeroType)
+       {
+            this.Name = Name; 
+            this.Level = Level; 
+            this.HeroType = HeroType;      
+       }
+      
+        public Hero()
+        {
+
+        }
+
+        public string Name; 
+        public int Level;
+        public string HeroType;
+
+
+        public override string ToString()
+        {
+         return this.Name + " " + this.Level + this.HeroType;   
+        }
+
+        public virtual string Attack()
+        {
+            return this.Name + "Atacou com a sua espada";
+        }
+
+    
+
+    }
+}
